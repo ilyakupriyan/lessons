ec80d9650438d016b136ef105e8e7dd696cf9bb5
Ilya <kupriyanov_2002@mail.ru>
2020-10-25 15:06:28 +0300

    The adding a division function

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 
 int main (void) {
 	int a, b = 0;
+	float div = 0;
 	puts ("Enter a and b");
 	scanf ("%d%d", &a, &b);
+	div = (float) a / b;
 	printf ("Sum is %d\n", a+b);
 	printf ("Multiplication is %d\n", a*b);
 	printf ("Subtraction is %d\n", a-b);
+	printf ("Division is %.3f", div);
 	return 0;
 }
