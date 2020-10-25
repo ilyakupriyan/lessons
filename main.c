12799c64dafb8c29c5eb35e7fc51efab37208f79
Ilya <kupriyanov_2002@mail.ru>
2020-10-25 13:57:01 +0300

    added mathematical functions
    The adding a multiplication function
    
    The adding a subtraction function
    
    The adding a division function
    
    The adding a test function

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+void test (int sum, int mult, int substr, float div);
+
 int main (void) {
-	printf ("Hello world");
+	int a, b, sum, mult, substr = 0;
+	float div = 0;
+	puts ("Enter a and b");
+	scanf ("%d%d", &a, &b);
+	sum = a + b;
+	mult = a*b;
+	substr = a - b;
+	div = (float) a/b;
+	test (sum, mult, substr, div);
 	return 0;
+}
+
+void test (int sum, int mult, int substr, float div) {
+	printf ("Summ is %d\n", sum);
+	printf ("Multiplication is %d\n", mult);
+	printf ("Substracion is %d\n", substr);
+	printf ("Division is %.3f\n", div);
 }
