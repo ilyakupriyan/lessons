a1b3029bc9bbd4068738ba9c6872dfb4314b9beb
Ilya <kupriyanov_2002@mail.ru>
2020-10-25 15:34:46 +0300

    The adding a test function

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
+void test (int sum, int mult, int substr, float div);
+
 int main (void) {
-	int a, b = 0;
+	int a, b, sum, mult, substr = 0;
 	float div = 0;
 	puts ("Enter a and b");
 	scanf ("%d%d", &a, &b);
-	div = (float) a / b;
-	printf ("Sum is %d\n", a+b);
-	printf ("Multiplication is %d\n", a*b);
-	printf ("Subtraction is %d\n", a-b);
-	printf ("Division is %.3f", div);
+	sum = a + b;
+	mult = a*b;
+	substr = a - b;
+	div = (float) a/b;
+	test (sum, mult, substr, div);
 	return 0;
+}
+
+void test (int sum, int mult, int substr, float div) {
+	printf ("Summ is %d\n", sum);
+	printf ("Multiplication is %d\n", mult);
+	printf ("Substracion is %d\n", substr);
+	printf ("Division is %.3f\n", div);
 }
