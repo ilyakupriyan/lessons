f9806ca7cd034f3f9fcd721b60ba70da5aaea863
Ilya <kupriyanov_2002@mail.ru>
2020-10-29 14:41:49 +0300

    Modification of program Array

--- array/array.c
+++ array/array.c
@@ -2,8 +2,13 @@
 
 int main (void) {
 	int array[10], count = 0;
-	for (int i = 0; i <=9; i++)
-		array[i] = 1;
+	for (int i = 0; i <=9; i++) {
+		array[i] = i % 2;
+		if (i == 4)
+ 			array[i] = 3;
+		else if (i == 5)
+			array[i] = 2;
+	}
 	while (count != 10)
 		printf ("%d ", array[count++]);
 	return 0;
